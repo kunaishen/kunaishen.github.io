20eac5c30000a35fe6bba7c1e715bf063507a2f7
kunaishen <aish3n@pm.me>
2022-02-07 15:02:13 +0800

    chore: github rename

--- deploy.sh
+++ deploy.sh
@@ -28,6 +28,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/kunaishen/kunaishen.github.io/ main:gh-pages
+git push -f https://github.com/aishenreemo/aishenreemo.github.io/ main:gh-pages
 
 cd -
