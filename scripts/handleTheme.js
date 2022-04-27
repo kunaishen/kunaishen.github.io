bd5ee74ec288a8e4beafea2ef4eed8a76101bdc4
aishenreemo <aish3n@pm.me>
2022-04-27 12:29:39 +0800

    feat: portfolio redesign

---
+++ scripts/handleTheme.js
@@ -0,0 +1,24 @@
+const themes = [
+    "dark_academic.css",
+    "kizu.css",
+];
+
+function changeTheme() {
+    let sheet = document.getElementById("theme");
+    sheet.dataset.id = parseInt(sheet.dataset.id) + 1;
+    sheet.href = "themes/" + themes[sheet.dataset.id % themes.length];
+    console.log("changed theme", sheet.href);
+}
+
+function initTheme() {
+    let sheet = document.createElement("link");
+
+    sheet.id = "theme";
+    sheet.rel  = "stylesheet";
+    sheet.type = "text/css";
+    sheet.href = "themes/" + themes[0];
+    sheet.dataset.id = 0;
+    document.getElementsByTagName('head')[0].appendChild(sheet);
+}
+
+initTheme();
