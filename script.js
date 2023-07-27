95dabb67acb4f9d7ca6152dd147ea7f00f1c56f1
Aishen <aish3n@pm.me>
2023-07-27 21:45:50 +0800

    feat: pagination

--- script.js
+++ script.js
@@ -7,10 +7,79 @@ document.addEventListener("mousemove", (e) => {
     const mouse = { x: e.clientX, y: e.clientY };
 
     const depths = [
-      `${50 - (mouse.x - width) * 0.06}% ${50 - (mouse.y - height) * 0.06}%`,
-      `${50 - (mouse.x - width) * 0.02}% ${50 - (mouse.y - height) * 0.02}%`,
-      `${50 - (mouse.x - width) * 0.01}% ${50 - (mouse.y - height) * 0.01}%`
+        `${50 - (mouse.x - width) * 0.06}% ${50 - (mouse.y - height) * 0.06}%`,
+        `${50 - (mouse.x - width) * 0.02}% ${50 - (mouse.y - height) * 0.02}%`,
+        `${50 - (mouse.x - width) * 0.01}% ${50 - (mouse.y - height) * 0.01}%`
     ];
 
     background.style.backgroundPosition = depths.join(", ");
+});
+
+const main = document.querySelector(".main");
+const pages_len = main.children.length;
+
+let is_wheel_cd = false;
+let page_index = 0;
+let start_swipe_y = 0;
+
+document.addEventListener("wheel", (e) => {
+    if (is_wheel_cd) return;
+    is_wheel_cd = true;
+
+    if (e.deltaY < 0) {
+        page_index -= 1;
+        if (page_index < 0) page_index = 0;
+    } else if (e.deltaY > 0) {
+        page_index += 1;
+        if (page_index == pages_len) page_index = pages_len - 1;
+    }
+
+    window.location.href = "#" + main.children[page_index].id;
+
+    enable = () => is_wheel_cd = false;
+    setTimeout(enable, 250);
+});
+
+document.addEventListener("touchstart", (e) => {
+    if (is_wheel_cd) return;
+    start_swipe_y = e.touches[0].clientY;
+});
+
+swipeArea.addEventListener("touchmove", (e) => {
+    if (is_wheel_cd) return;
+    is_wheel_cd = true;
+
+    const current_y = e.touches[0].clientY;
+    const delta_y = current_y - start_swipe_y;
+
+    if (delta_y > 0) {
+        page_index -= 1;
+        if (page_index < 0) page_index = 0;
+    } else if (delta_y < 0) {
+        page_index += 1;
+        if (page_index == pages_len) page_index = pages_len - 1;
+    }
+
+    window.location.href = "#" + main.children[page_index].id;
+
+    enable = () => is_wheel_cd = false;
+    setTimeout(enable, 250);
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute("href").slice(1);
+        const targetElement = document.getElementById(targetId);
+
+        for (let i = 0; i < pages_len; i++) {
+            if (main.children[i].id != targetId) continue;
+            page_index = i;
+        }
+
+
+        if (!targetElement) return;
+
+        targetElement.scrollIntoView({ behavior: "smooth" });
+    });
 });
