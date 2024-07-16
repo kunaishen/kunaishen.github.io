5e279a55db4657ddf751acefb9acd9b06d70efc0
Aivan Ross Anuyo <aish3n@pm.me>
2024-07-16 18:39:46 +0800

    feat: spinning cirno

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 import * as three from 'three';
+import cirball from './cirball.png';
 
 const scene = new three.Scene();
 const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new three.WebGLRenderer({ canvas: document.getElementById("canvas") });
-const geometry = new three.BoxGeometry(1, 1, 1);
-const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new three.Mesh(geometry, material);
+const ambientLight = new three.AmbientLight(0xffffff);
+const nineball = new three.Mesh(
+    new three.SphereGeometry(10, 32, 32),
+    new three.MeshStandardMaterial({
+        map: new three.TextureLoader().load(cirball)
+    })
+);
+
+console.log(nineball);
 
 function main() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setAnimationLoop(animate);
 
-    scene.add(cube);
+    scene.add(ambientLight);
+    scene.add(nineball);
 
-    camera.position.z = 5;
+    camera.position.z = 30;
 }
 
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    requestAnimationFrame(animate);
+    nineball.rotation.y += 0.0001;
 
     renderer.render(scene, camera);
 }
